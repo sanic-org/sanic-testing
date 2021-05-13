36c0864a317b4db1178578e60a909f0f088f5dba
Adam Hopkins <admhpkns@gmail.com>
2021-05-13 11:53:30 +0300

    Cleanup code

--- sanic_testing/manager.py
+++ sanic_testing/manager.py
@@ -5,7 +5,7 @@ from sanic_testing.testing import SanicASGITestClient, SanicTestClient
 
 class TestManager:
     __test__ = False
-    
+
     def __init__(self, app: Sanic) -> None:
         self.test_client = SanicTestClient(app)
         self.asgi_client = SanicASGITestClient(app)
