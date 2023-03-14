4b2aaafb47a6ff297211d27be434a85d417a25ca
L. Kärkkäinen <98187+Tronic@users.noreply.github.com>
2023-03-14 17:02:20 +0000

    Update testing.py (#64)

--- sanic_testing/testing.py
+++ sanic_testing/testing.py
@@ -446,6 +446,7 @@ class SanicASGITestClient(httpx.AsyncClient):
             "scheme": scheme,
             "root_path": root_path,
             "path": path,
+            "raw_path": path.encode(),
             "query_string": b"",
             "subprotocols": subprotocols,
         }
